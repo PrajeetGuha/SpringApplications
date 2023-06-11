589d49f4ebb3b04a6d593cf1a3c204b1aab06e70
Prajeet Guha <58776463+PrajeetGuha@users.noreply.github.com>
2023-06-11 09:01:07 +0530

    Delete ems directory

--- ems/src/main/java/org/antwalk/ems/repository/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package org.antwalk.ems.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.antwalk.ems.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee,Long> {
-}
